9e193ad0617b35a6ee30d7d590177505677e4511
221710316035 <70636725+221710316035@users.noreply.github.com>
2020-09-09 15:28:11 +0530

    Add files via upload

---
+++ C#-2/series.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            for (int j = 0; j < 10; ++j) {
+                for (int i = 0; i < j; ++i)
+                    Console.Write(7);
+                Console.WriteLine();
+            }
+        }
+    }
+}
