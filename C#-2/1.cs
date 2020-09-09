4529013d5c2250672071907f40ab31a2af693569
221710316035 <70636725+221710316035@users.noreply.github.com>
2020-09-09 15:05:33 +0530

    Add files via upload

---
+++ C#-2/1.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+		Console.WriteLine(cars[0]);
+        }
+    }
+}
